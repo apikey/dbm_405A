ba7136adc6013496b0e3e2aaeebf1a5eca9d96e7
apikey <jasonmfranco@gmail.com>
2015-06-13 16:43:32 -0500

    add events.sql

---
+++ events.sql
@@ -0,0 +1,34 @@
+-- Set up an event to back up your database at midnight every Friday night.
+DELIMITER ;;
+CREATE EVENT BACKUP_EVERY_FRIDAY
+    ON SCHEDULE EVERY 1 WEEK
+        STARTS TIMESTAMP(CASE DAYNAME(NOW())
+            WHEN 'Friday' THEN NOW()
+            WHEN 'Saturday' THEN NOW + INTERVAL 6 DAY
+            WHEN 'Sunday' THEN NOW + INTERVAL 5 DAY
+            WHEN 'Monday' THEN NOW + INTERVAL 4 DAY
+            WHEN 'Tuesday' THEN NOW + INTERVAL 3 DAY
+            WHEN 'Wednesday' THEN NOW + INTERVAL 2 DAY
+            WHEN 'Thursday' THEN NOW + INTERAL 1 DAY
+            END, '00:00:00')
+        DO
+        BEGIN
+            SELECT * INTO OUTFILE 'current_schedule.csv'
+            FROM baseball.current_schedule;
+            
+            SELECT * INTO OUTFILE 'pastseasons.csv'
+            FROM baseball.pastseasons;
+            
+            SELECT * INTO OUTFILE 'player_team.csv'
+            FROM baseball.player_team;
+            
+            SELECT * INTO OUTFILE 'players.csv'
+            FROM baseball.players;
+            
+            SELECT * INTO OUTFILE 'season.csv'
+            FROM baseball.season;
+            
+            SELECT * INTO OUTFILE 'teams.csv'
+            FROM baseball.season;
+        END;
+
